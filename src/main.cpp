5eb9a4042219d3047c71acd7316c241e8d638f7e
SaltySnail <julianstaphorsius@hotmail.com>
2023-11-01 16:52:07 +0000

    Test plotter in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "HarryPlotter"
 
 int main() {
+    Plotter plot;
+    plot.AddPoint({0,0});
+    plot.AddPoint({1,1});
+    plot.AddPoint({2,2});
+    plot.AddPoint({3,3});
+    plot.AddPoint({4,4});
+    plot.ExportAsPNG();
     return 0;
 }
