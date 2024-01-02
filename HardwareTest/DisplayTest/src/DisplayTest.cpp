4b3582f6f01c008a8b860a513ff38f1b7d6c3bc6
SaltySnail <julianstaphorsius@hotmail.com>
2024-01-02 23:40:03 +0100

    Test cleanup

--- HardwareTest/DisplayTest/src/DisplayTest.cpp
+++ HardwareTest/DisplayTest/src/DisplayTest.cpp
@@ -47,43 +47,8 @@ int main(){
 
   display.framebuffer_clear(frameBuff);
 
-  // display.draw_char(frameBuff,frameBuff->width/2, frameBuff->height/2, 'K');
-  // ssd1306_i2c_display_update(oled, frameBuff);
-  // sleep(3);
-
-  // display.framebuffer_clear(frameBuff);
-  // ssd1306_i2c_display_update(oled, frameBuff);
-
-  // display.draw_char(frameBuff,frameBuff->width/2, frameBuff->height/2, 'L');
-  // ssd1306_i2c_display_update(oled, frameBuff);
-  // sleep(3);
-
-  // display.framebuffer_clear(frameBuff);
-  // ssd1306_i2c_display_update(oled, frameBuff);
-
-  // display.draw_char(frameBuff,frameBuff->width/2, frameBuff->height/2, 'M');
-  // ssd1306_i2c_display_update(oled, frameBuff);
-  // sleep(3);
-  // // display.framebuffer_put_pixel(frameBuff, 0, 0, true);
-  // // display.framebuffer_put_pixel(frameBuff, frameBuff->width - 1, 0, true);
-  // // display.framebuffer_put_pixel(frameBuff, 0, frameBuff->height - 1, true);
-  // // display.framebuffer_put_pixel(frameBuff, frameBuff->width - 1, frameBuff->height - 1, true);
-  // // display.framebuffer_put_pixel(frameBuff, 9, 10, true);
-
-  // // ssd1306_i2c_display_update(oled, frameBuff);
-  // // sleep(3);
-  // // display.framebuffer_clear(frameBuff);
-  // // display.framebuffer_put_pixel(frameBuff, 0, 0, false);
-  // // display.framebuffer_put_pixel(frameBuff, frameBuff->width - 1, 0, false);
-  // // display.framebuffer_put_pixel(frameBuff, 0, frameBuff->height - 1, false);
-  // // display.framebuffer_put_pixel(frameBuff, frameBuff->width - 1, frameBuff->height - 1, false);
-  // // display.framebuffer_put_pixel(frameBuff, 9, 10, false);
-  // // ssd1306_i2c_display_update(oled, frameBuff);
-  // // display.framebuffer_clear(frameBuff);
-  // // sleep(3);
-
-  // display.Draw(drawing);
-  // sleep(10);
+  display.Draw(drawing);
+  sleep(5);
   
   display.Print("Hello world!");
   sleep(5);
