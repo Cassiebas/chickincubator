d5fb453d9c22f4c14aacc41332d1b9dcef521c17
Cassiebas <casperleitner@outlook.com>
2024-01-11 16:56:43 +0100

    Removed Hardwaretest.

--- HardwareTest/OnOffTest/src/OnOffTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "OnOffController.hpp"
-
-int main() {
-    OnOffController controller(37.5); //37.5 °C
-    controller.Start();
-    while (controller.GetTime()/60.0 < 45.0);
-    controller.Stop();
-    return 0;
-}
